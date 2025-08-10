6f8c4475dc7a331703402a0d47133733ebccbedb
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-08-10 21:32:15 +0200

    Allow generating blobs from offscreen canvasses

--- src/util/canvasToBlob.ts
+++ src/util/canvasToBlob.ts
@@ -1,15 +1,12 @@
+import { Canvas } from "../lib/constants";
+
 const blobMimes = [ "image/webp", "image/png" ];
 
-export async function getBlobFromCanvas(canvas: HTMLCanvasElement): Promise<[Blob, string]> {
+export async function getBlobFromCanvas(canvas: Canvas): Promise<[Blob, string]> {
     const result = (await Promise.allSettled(blobMimes.map(mime =>
-        new Promise<Blob>((resolve) =>
-            canvas.toBlob(blob => {
-                if (blob === null) {
-                    throw new Error(`Failed to extract data as ${mime}`);
-                }
-                resolve(blob);
-            }, mime, 1.)
-        )
+        canvas instanceof HTMLCanvasElement ?
+            fromHTMLCanvas(canvas, mime) :
+            fromOffscreenCanvas(canvas, mime)
     ))).filter(result => result.status === "fulfilled")[0];
 
     if (!result) {
@@ -18,4 +15,19 @@ export async function getBlobFromCanvas(canvas: HTMLCanvasElement): Promise<[Blo
 
     const blob = result.value;
     return [blob, (blob.type.split("/")[1])];
+}
+
+function fromHTMLCanvas(canvas: HTMLCanvasElement, mimeType: string): Promise<Blob> {
+    return new Promise<Blob>((resolve) =>
+        canvas.toBlob(blob => {
+            if (blob === null) {
+                throw new Error(`Failed to extract data as ${mimeType}`);
+            }
+            resolve(blob);
+        }, mimeType, 1.)
+    );
+}
+
+function fromOffscreenCanvas(canvas: OffscreenCanvas, mimeType: string): Promise<Blob> {
+    return canvas.convertToBlob({ type: mimeType, quality: 1. });
 }
